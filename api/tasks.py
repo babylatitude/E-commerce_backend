2f70b49edb18855305ec7bb482a7ac0aef04ca75
kali <babylatitude@gmail.com>
2025-07-20 05:22:09 +0100

    Initial commit

---
+++ api/tasks.py
@@ -0,0 +1,10 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+@shared_task
+def send_order_confirmation_email(order_id, user_email):
+    subject = "Order Confirmation"
+    message = f"Your order with order ID {order_id} has been confirmed and is been proccessed"
+    return send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user_email])
---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from rest_framework.routers import DefaultRouter
+
+urlpatterns = [
+    path('products/', views.ProductListCreateApiView.as_view()),
+    path('products/info/', views.ProductInfoApiView.as_view()),
+    path('products/<int:product_id>/',
+         views.ProductDetailApiView.as_view(), name='product-detail'),
+    path('users/', views.UserListView.as_view()),
+]
+
+router = DefaultRouter()
+router.register('orders', views.OrderViewSet)
+urlpatterns += router.urls
